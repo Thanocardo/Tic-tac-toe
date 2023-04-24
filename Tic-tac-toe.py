d14de1f6e2d5fcf10d3e27e8931277abfbcc57c6
Martin Neychev <marti.neychev@gmail.com>
2023-04-24 15:27:58 +0300

    bug fix and optimisation.

--- Tic-tac-toe.py
+++ Tic-tac-toe.py
@@ -3,17 +3,24 @@ def tic_tac_toe_board(positions):
     print(f"{positions['4']} {positions['5']} {positions['6']}     '4 5 6'")
     print(f"{positions['1']} {positions['2']} {positions['3']}     '1 2 3'")
 
-def tic_tac_toe_check(player, positions):
-    for number in range(1,8,3):
-        if positions[str(number)] == player and positions[str(number+1)] == player and positions[str(number+2)] == player:
-            return True
-    for number in range(1,4):
-        if positions[str(number)] == player and positions[str(number+3)] == player and positions[str(number+6)] == player:
-            return True
-    if positions["1"] == player and positions["5"] == player and positions["9"] == player:
+def tic_tac_toe_check(player, positions, score):
+    if (positions["1"] == player and positions["2"] == player and positions["3"] == player) or \
+        (positions["4"] == player and positions["5"] == player and positions["6"] == player) or \
+        (positions["7"] == player and positions["8"] == player and positions["9"] == player) or \
+        (positions["1"] == player and positions["4"] == player and positions["7"] == player) or \
+        (positions["2"] == player and positions["5"] == player and positions["8"] == player) or \
+        (positions["3"] == player and positions["6"] == player and positions["9"] == player) or \
+        (positions["1"] == player and positions["5"] == player and positions["9"] == player) or \
+        (positions["3"] == player and positions["5"] == player and positions["7"] == player):
+        score[player] += 1
+        print(f"{player} WIN!!!")
         return True
-    elif positions["3"] == player and positions["5"] == player and positions["7"] == player:
+    elif positions["1"] != "-" and positions["2"] != "-" and positions["3"] != "-" and \
+            positions["4"] != "-" and positions["5"] != "-" and positions["6"] != "-" and \
+            positions["7"] != "-" and positions["8"] != "-" and positions["9"] != "-":
+        print("DRAW")
         return True
+
 
 score = {"X": 0, "O": 0}
 positions = {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-", "7": "-", "8": "-", "9": "-"}
@@ -35,12 +42,10 @@ while True:
             print("Invalid position, please try again.")
             continue
 
-    if tic_tac_toe_check(player, positions):
+    if tic_tac_toe_check(player, positions, score):
         tic_tac_toe_board(positions)
-        print(f"{player} WIN!!!")
-        score[player] += 1
         while True:
-            again = input("Wanna play again? [YES/NO] ")
+            again = input("Wanna play again? [YES/NO]: ")
             again = again.lower()
             if again == "yes":
                 positions = {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-", "7": "-", "8": "-", "9": "-"}
